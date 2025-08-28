0391fcb948bbb22b9dcf2d6f90218e5db356ef9f
ElGonan <alanpagzz@outlook.com>
2025-08-28 11:16:27 -0600

    form type assured

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,40 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
+import type { Activity } from "../types"
 
 export const Form = () => {
-  return (
+    
+    // En lugar de tener un state para cada elemento, ya que están conectados, usaremos un objeto
+    // con todos los elementos que requerimos para hacer fucionar este forms.
+    const [activity, setActivity] = useState<Activity>({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
+    // Aunque esto se podría poner inline en el onChange, sería mucho códgio reescrito, mejor
+    // una función nueva. Es más limpio.
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
+
+        console.log(isNumberField)
+        
+
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    return (
     <>
         <form className='space-y-5 bg-white shadow p-10 rounded-lg'>
             <div className='grid grid-cols-1 gap-3'>
                 <label htmlFor='category' className="font-bold">Categoría:</label>
-                <select className='border border-slate-300 p-2 rounded-kg w-full bg-white' id="category">
+                <select className='border border-slate-300 p-2 rounded-kg w-full bg-white' id="category"
+                value={activity.category}
+                onChange={handleChange }>
                     {categories.map(category => (
                         <option
                         key={category.id}
@@ -18,12 +46,14 @@ export const Form = () => {
             </div>
 
             <div className='grid grid-cols-1 gap-3'>
-                <label htmlFor='activity' className="font-bold">Actividad:</label>
+                <label htmlFor='name' className="font-bold">Actividad:</label>
                 <input
-                id="activity"
+                id="name"
                 type="text"
                 className="border border-slate-300 p-2 rounded-lg"
-                placeholder="Ej. Comida, jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"/>
+                placeholder="Ej. Comida, jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+                value={activity.name}
+                onChange={handleChange}/>
 
 
             </div>
@@ -34,14 +64,17 @@ export const Form = () => {
                 id="calories"
                 type="number"
                 className="border border-slate-300 p-2 rounded-lg"
-                placeholder="Ej: 300,500"/>
+                placeholder="Ej: 300,500"
+                value={activity.calories}
+                onChange={handleChange}/>
 
 
             </div>
 
             <input
             type="submit"
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer transition-colors">
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer transition-colors"
+            value="Guardar comida o ejericicio">
             </input>
     
 

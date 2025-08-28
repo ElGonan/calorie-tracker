4dfc7ee7007c4c72b738176701eb623e9bd8ef1b
ElGonan <alanpagzz@outlook.com>
2025-08-28 10:41:15 -0600

    form completed

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,15 +5,44 @@ export const Form = () => {
     <>
         <form className='space-y-5 bg-white shadow p-10 rounded-lg'>
             <div className='grid grid-cols-1 gap-3'>
-                <label htmlFor=''>Categoría:</label>
+                <label htmlFor='category' className="font-bold">Categoría:</label>
                 <select className='border border-slate-300 p-2 rounded-kg w-full bg-white' id="category">
                     {categories.map(category => (
-                        <option>
+                        <option
+                        key={category.id}
+                        value={category.id}>
                             {category.name}
                         </option>
                     ))}
                 </select>
             </div>
+
+            <div className='grid grid-cols-1 gap-3'>
+                <label htmlFor='activity' className="font-bold">Actividad:</label>
+                <input
+                id="activity"
+                type="text"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"/>
+
+
+            </div>
+
+            <div className='grid grid-cols-1 gap-3'>
+                <label htmlFor='calories' className="font-bold">Calorías:</label>
+                <input
+                id="calories"
+                type="number"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej: 300,500"/>
+
+
+            </div>
+
+            <input
+            type="submit"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer transition-colors">
+            </input>
     
 
         </form>
